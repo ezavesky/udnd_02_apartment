f77dd59b3a50f9806348b8aedf95596c0eaacf48
Eric Zavesky <ezavesky@research.att.com>
2017-09-10 06:14:09 -0500

    - minor updates (static objects, bundle identifier) updates for code review

---
+++ Assets/MarketExtra/FilterNonStaticObjects.cs
@@ -0,0 +1,33 @@
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FilterNonStaticObjects : EditorWindow
+{
+    [MenuItem( "Custom/Select Non-Static" )]
+    static void Init()
+    {
+        Object[] gameObjects = FindObjectsOfType( typeof ( GameObject ) );
+
+        GameObject[] gameObjectArray;
+        gameObjectArray = new GameObject[ gameObjects.Length ];
+
+        int arrayPointer = 0;
+
+        foreach ( GameObject gameObject in gameObjects )
+        {
+            StaticEditorFlags flags = GameObjectUtility.GetStaticEditorFlags( gameObject );
+
+            if ( ( flags & StaticEditorFlags.LightmapStatic ) == 0 )
+            {
+                gameObjectArray[ arrayPointer ] = gameObject;
+                arrayPointer += 1;
+            }
+        }
+
+        Selection.objects = gameObjectArray;
+
+    }
+}
+#endif
